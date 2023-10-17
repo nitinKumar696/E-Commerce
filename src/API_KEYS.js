67a56428e40c4180f5588059d9b64b85fed4123d
nitinKumar696 <148214692+nitinKumar696@users.noreply.github.com>
2023-10-17 16:38:18 +0530

    Add files via upload

---
+++ src/API_KEYS.js
@@ -0,0 +1,5 @@
+const headers = {
+  "X-RapidAPI-Key": "613cf127f5msh97949d8491e5c62p1a7131jsn765e4eeb8335",
+  "X-RapidAPI-Host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com",
+};
+export { headers };
